bcefc0d63e07da556afe71eb28f59259964b797f
Duc <ddquach14@gmail.com>
2021-12-13 11:16:39 +0700

    add test

---
+++ src/musicpj/YouTubeViewer.java
@@ -0,0 +1,43 @@
+package musicpj;
+
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class YouTubeViewer {
+
+    public static void main(String[] args) {
+        NativeInterface.open();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("YouTube Viewer");
+                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                frame.getContentPane().add(getBrowserPanel(), BorderLayout.CENTER);
+                frame.setSize(800, 600);
+                frame.setLocationByPlatform(true);
+                frame.setVisible(true);
+            }
+        });
+        NativeInterface.runEventPump();
+        // don't forget to properly close native components
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+                NativeInterface.close();
+            }
+        }));
+    }
+
+    public static JPanel getBrowserPanel() {
+        JPanel webBrowserPanel = new JPanel(new BorderLayout());
+        JWebBrowser webBrowser = new JWebBrowser();
+        webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+        webBrowser.setBarsVisible(false);
+        webBrowser.navigate("https://www.youtube.com/watch?v=BXB26PzV31k");
+        return webBrowserPanel;
+    }
+}
